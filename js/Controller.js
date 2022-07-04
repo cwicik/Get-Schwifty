4b4ecdc646537fd75a063fe02d3f4aa7213fdd68
Daniel Gladkov <daniel.gladkov@gmail.com>
2022-07-04 10:03:02 +0300

    Working basic game

---
+++ js/Controller.js
@@ -0,0 +1,108 @@
+class Tile{
+    constructor(number){
+        this.number = number;
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+class Board{
+    constructor(size){
+        this.size = size;
+        this.emptyTile = size * size;
+        this.board = null;
+        do {
+            this.generateBoard();
+        }while(!this.isValid() || this.isSolved());
+    }
+
+    generateBoard(){
+        this.board = new Array();
+
+        const numbers = [...Array(this.size * this.size).keys()];
+        numbers.forEach(num => numbers[num]++);
+
+        for (let x = 0; x < this.size; x++) {
+            const column = new Array();
+            for (let y = 0; y < this.size; y++) {
+                const number = numbers[getRandomInt(numbers.length)];
+                column.push(new Tile(number));
+
+                const index = numbers.indexOf(number);
+                numbers.splice(index, 1);
+            }
+            this.board.push(column);
+        } 
+    }
+
+    isValid(){
+        const seen = new Array(); 
+        let count = 0;
+        for (let x = 0; x < this.size; x++) {
+            for (let y = 0; y < this.size; y++) {
+                if(this.isEmptyTile(y,x)) {
+                    if ((this.size + 1) % 2) {
+                        count += y + 1;
+                    }
+                }
+                else{
+                    const currentNum = this.board[y][x].number;
+                    seen.push(currentNum);
+                    count += currentNum - seen.filter(num => num <= currentNum).length;
+                }    
+            }
+        }
+        return !Boolean(count % 2);
+    }
+
+    isSolved(){
+        let lastNumber = null;
+        for (let x = 0; x < this.size; x++) {
+            for (let y = 0; y < this.size; y++) {
+                const currentNum = this.board[y][x].number;
+                if (lastNumber) {
+                    if (lastNumber + 1 !== currentNum) {
+                        return false;
+                    }
+                }
+                lastNumber = currentNum;
+            }
+        }
+        return true;
+    }
+
+    attemptSwitchTile(x, y){
+        if (x + 1 < board.size) {
+            if (this.isEmptyTile(x + 1, y)) {
+                return this.switchTiles(x,y, x+1,y);
+            }
+        }
+        if (x - 1 >= 0) {
+            if (this.isEmptyTile(x - 1, y)) {
+                return this.switchTiles(x,y, x-1,y);
+            }
+        }
+        if (y + 1 < board.size) {
+            if (this.isEmptyTile(x, y + 1)) {
+                return this.switchTiles(x,y, x,y+1);
+            }
+        }
+        if (y - 1 >= 0) {
+            if (this.isEmptyTile(x, y - 1)) {
+                return this.switchTiles(x,y, x,y-1);
+            }
+        }
+        return false;
+    }
+
+    isEmptyTile(x,y){
+        return this.board[x][y].number === this.emptyTile;
+    }
+
+    switchTiles(x1,y1, x2,y2){
+            [this.board[x1][y1], this.board[x2][y2]] = [this.board[x2][y2], this.board[x1][y1]];
+            return true;
+    }
+}
---
+++ js/View.js
@@ -0,0 +1,49 @@
+class BoardDisplayer{
+    constructor() {
+        this.buttons = new Array();
+    }    
+
+    displayBoard(){
+        for (let y = 0; y < board.size; y++) {
+            const div = document.createElement("div");
+            for (let x = 0; x < board.size; x++) {
+                const button = document.createElement("button");
+                button.innerHTML = board.board[x][y].number === board.emptyTile ? "&nbsp;" : board.board[x][y].number;
+                button.addEventListener("click", () => {
+                    board.attemptSwitchTile(x, y);
+                    this.updateBoard();
+                    board.isSolved() ? alert("won pog") : null;
+                }
+                );
+                div.appendChild(button);      
+                this.buttons.push(button);   
+            }
+            document.body.appendChild(div);
+        }
+    }
+
+    updateBoard(){
+        for (let y = 0; y < board.size; y++) {
+            for (let x = 0; x < board.size; x++) {
+                const button = this.buttons[y * board.size + x];
+                button.innerHTML = board.board[x][y].number === board.emptyTile ? "&nbsp;" : board.board[x][y].number; 
+            }
+        }
+    }
+}
+
+class BoardBootstrapper{
+    createBoard(){
+        do {
+            var boardSize = Number.parseInt(prompt("Please enter board size", 3));
+        } while (!boardSize || boardSize < 2);
+        return new Board(boardSize);
+    }
+}
+
+
+const boardBootstrapper = new BoardBootstrapper();
+const board = boardBootstrapper.createBoard();
+
+const boardDisplayer = new BoardDisplayer();
+boardDisplayer.displayBoard();
